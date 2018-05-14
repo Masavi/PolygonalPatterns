a97f35970dd0d41bd45b9f38e881d0b9d9a6f096
Masavi <mauriciosaavedra2005@gmail.com>
2018-05-14 15:59:52 -0500

    add input fields for coordinates and js main file

---
+++ main.js
@@ -0,0 +1,145 @@
+/*
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(50, 150);
+ctx.stroke();
+
+
+//poly [x,y, x,y, x,y.....];
+var poly=[ 5,5, 100,50, 50,100, 10,90 ];
+var canvas=document.getElementById("myCanvas")
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = '#f00';
+
+ctx.beginPath();
+ctx.moveTo(poly[0], poly[1]);
+for( item=2 ; item < poly.length-1 ; item+=2 ){ctx.lineTo( poly[item] , poly[item+1] )}
+
+ctx.closePath();
+ctx.fill();
+*/
+
+var i = 0; /* Set Global Variable i */
+function increment(){
+i += 1; /* Function for automatic increment of field's "Name" attribute. */
+}
+/*
+---------------------------------------------
+
+Function to Remove Form Elements Dynamically
+---------------------------------------------
+
+*/
+function removeElement(parentDiv, childDiv){
+if (childDiv == parentDiv){
+alert("The parent div cannot be removed.");
+}
+else if (document.getElementById(childDiv)){
+var child = document.getElementById(childDiv);
+var parent = document.getElementById(parentDiv);
+parent.removeChild(child);
+}
+else{
+alert("Child div has already been removed or does not exist.");
+return false;
+}
+}
+/*
+----------------------------------------------------------------------------
+
+Functions that will be called upon, when user click on the Name text field.
+
+----------------------------------------------------------------------------
+*/
+function nameFunction(){
+var r = document.createElement('span');
+var y = document.createElement("INPUT");
+y.setAttribute("type", "text");
+y.setAttribute("placeholder", "Name");
+var g = document.createElement("IMG");
+g.setAttribute("src", "delete.png");
+increment();
+y.setAttribute("Name", "textelement_" + i);
+r.appendChild(y);
+g.setAttribute("onclick", "removeElement('myForm','id_" + i + "')");
+r.appendChild(g);
+r.setAttribute("id", "id_" + i);
+document.getElementById("myForm").appendChild(r);
+}
+/*
+-----------------------------------------------------------------------------
+
+Functions that will be called upon, when user click on the E-mail text field.
+
+------------------------------------------------------------------------------
+*/
+function emailFunction(){
+var r = document.createElement('span');
+var y = document.createElement("INPUT");
+y.setAttribute("type", "text");
+y.setAttribute("placeholder", "Email");
+var g = document.createElement("IMG");
+g.setAttribute("src", "delete.png");
+increment();
+y.setAttribute("Name", "textelement_" + i);
+r.appendChild(y);
+g.setAttribute("onclick", "removeElement('myForm','id_" + i + "')");
+r.appendChild(g);
+r.setAttribute("id", "id_" + i);
+document.getElementById("myForm").appendChild(r);
+}
+/*
+-----------------------------------------------------------------------------
+
+Functions that will be called upon, when user click on the Contact text field.
+
+------------------------------------------------------------------------------
+*/
+function contactFunction(){
+var r = document.createElement('span');
+var y = document.createElement("INPUT");
+y.setAttribute("type", "text");
+y.setAttribute("placeholder", "Contact");
+var g = document.createElement("IMG");
+g.setAttribute("src", "delete.png");
+increment();
+y.setAttribute("Name", "textelement_" + i);
+r.appendChild(y);
+g.setAttribute("onclick", "removeElement('myForm','id_" + i + "')");
+r.appendChild(g);
+r.setAttribute("id", "id_" + i);
+document.getElementById("myForm").appendChild(r);
+}
+/*
+-----------------------------------------------------------------------------
+
+Functions that will be called upon, when user click on the Message textarea field.
+
+------------------------------------------------------------------------------
+*/
+function textareaFunction(){
+var r = document.createElement('span');
+var y = document.createElement("TEXTAREA");
+var g = document.createElement("IMG");
+y.setAttribute("cols", "17");
+y.setAttribute("placeholder", "message..");
+g.setAttribute("src", "delete.png");
+increment();
+y.setAttribute("Name", "textelement_" + i);
+r.appendChild(y);
+g.setAttribute("onclick", "removeElement('myForm','id_" + i + "')");
+r.appendChild(g);
+r.setAttribute("id", "id_" + i);
+document.getElementById("myForm").appendChild(r);
+}
+/*
+-----------------------------------------------------------------------------
+
+Functions that will be called upon, when user click on the Reset Button.
+
+------------------------------------------------------------------------------
+*/
+function resetElements(){
+document.getElementById('myForm').innerHTML = '';
+}
